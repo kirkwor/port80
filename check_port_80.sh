591f857df359df8accc49d6baf86aa95cbe35c04
Ionut Bochis <ionut.bochis@gmail.com>
2016-09-29 18:36:02 -0400

    Added check for IP/class and comments

--- check_port_80.sh
+++ check_port_80.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#Simple cat in a list and skip output lines starting with #
 for i in `cat list | grep -v "#"` ; do
-curl -s $i:80 >/dev/null && echo "$i is Up" || echo "$i is Down" ; done
+
+#@ This is useful if NO IP/class is present!
+#curl -s $i:80 >/dev/null && echo "$i is Up" || echo "$i is Down" ; done
+
+#@ This is useful if an IP/class is present!
+echo Checking $i ; nmap -oG - -p 80 -Pn $i | grep http ; done
